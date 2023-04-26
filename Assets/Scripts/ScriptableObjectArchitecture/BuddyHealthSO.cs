344185b5e2621382996eb4ded2ceff32525fd6e9
miguelguti <miguelgutierrezruano@gmail.com>
2023-04-26 14:16:14 +0200

    scene set up

---
+++ Assets/Scripts/ScriptableObjectArchitecture/BuddyHealthSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuddyHealthSO : MonoBehaviour
+{
+    // Health variable
+    [SerializeField] private IntVariable health;
+    
+    
+}
